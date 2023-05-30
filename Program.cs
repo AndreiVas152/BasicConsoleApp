3c7c945b43d32ed9e5262f6fd03a0b9837d52d6a
AndreiVas152 <135043101+AndreiVas152@users.noreply.github.com>
2023-05-30 18:15:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cum te cheama ?");
+            var UserInput = Console.ReadLine();
+            Console.WriteLine($"Salut, {UserInput}");
+            Console.ReadLine();
+        }
+    }
+}
